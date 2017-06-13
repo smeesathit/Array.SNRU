1cfea5c56da2e21a497439d165a9e9f61530019c
student04 <smeesathit@gmail.com>
2017-06-13 15:07:06 +0700

    Optional

--- Optional.playground/Contents.swift
+++ Optional.playground/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Optional  = variable that its value can be null
+var intNumber: Int?
+print("intNumber ==> \(intNumber)")
+
+// Using Optional
+var strNumber = "5"
+
+strNumber = "10"
+//strNumber="Ten"
+
+var intNumber1 = Int(strNumber)
+
+if var intMyNumber = intNumber1 {
+    var myAnswer = intMyNumber
+    print("myAnswer * 2 ==> \(myAnswer * 2)")
+}
+
+//print("intNumber1 ==> \(intNumber1)")
+//print("intNumber1 * 2 ==> \(intNumber1! * 2)")
+
