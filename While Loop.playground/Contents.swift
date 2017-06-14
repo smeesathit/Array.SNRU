8ff21b69f3de1e9483b269af9d69fe5d5f09ee59
student04 <smeesathit@gmail.com>
2017-06-14 14:07:32 +0700

    While Loop

---
+++ While Loop.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var intTimes = 1
+
+while intTimes <= 10 {
+    print("intTimes ==> \(intTimes)" )
+    intTimes += 1
+}
+
+// Multiplication table for 2
+var intTimes2 = 1
+while intTimes2 <= 12 {
+    
+    print("2 x \(intTimes2) = \(2 * intTimes2)")
+    intTimes2 += 1
+    
+}
+
+let strArrayBook = ["Math", "Physic" ,"Chem" ,"Bio" ,"Computer", "English"]
+var intIndexBook = 0
+while intIndexBook < strArrayBook.count {
+    print("This is a \(strArrayBook[intIndexBook]) book.")
+    intIndexBook += 1
+}
+
